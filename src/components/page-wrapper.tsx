5892fd95d116c485dd549218ad816e5247170a45
Lucas Faria <lucasheriques@gmail.com>
2024-12-06 13:42:50 -0300

    pb update

--- src/components/page-wrapper.tsx
+++ src/components/page-wrapper.tsx
@@ -1,3 +1,3 @@
 export function PageWrapper({ children }: { children: React.ReactNode }) {
-  return <div className="content-wrapper pt-24">{children}</div>;
+  return <div className="content-wrapper pt-24 pb-20">{children}</div>;
 }
