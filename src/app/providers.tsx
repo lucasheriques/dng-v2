940a7bd005a4a744a6a8b84349473060e0f21a3f
Lucas Faria <lucasheriques@gmail.com>
2025-04-16 13:03:26 -0300

    stop async posthog load

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,8 +8,9 @@ import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 import { ConvexQueryCacheProvider } from "convex-helpers/react/cache/provider";
 import { ConvexReactClient } from "convex/react";
 import { LazyMotion } from "motion/react";
-import dynamic from "next/dynamic";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
 import { Provider as BalancerProvider } from "react-wrap-balancer";
 
@@ -17,22 +18,13 @@ const loadFeatures = import("../components/framer-features").then(
   (res) => res.default
 );
 
-const PostHogProvider = dynamic(
-  () => import("posthog-js/react").then((mod) => mod.PostHogProvider),
-  {
-    ssr: false,
-  }
-);
-
-const asyncPosthog = await import("posthog-js");
-
 const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL);
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const isDev = process.env.NODE_ENV === "development";
 
   useEffect(() => {
-    asyncPosthog.posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: "identified_only",
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
@@ -44,7 +36,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <LazyMotion features={async () => await loadFeatures} strict>
-      <PostHogProvider client={asyncPosthog.posthog}>
+      <PostHogProvider client={posthog}>
         <ConvexAuthNextjsProvider client={convex}>
           <ConvexQueryCacheProvider>
             <NuqsAdapter>
